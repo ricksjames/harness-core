ca98a199551595d67f201345173f3e4c857aeffd
Richa Jajoo <richa.jajoo@harness.io>
2022-03-28 16:10:02 +0530

    PL-22547: fix portal-openjdk-bazel.sh

--- scripts/jenkins/portal-openjdk-bazel.sh
+++ scripts/jenkins/portal-openjdk-bazel.sh
@@ -12,6 +12,7 @@ source "${SCRIPT_DIR}/portal-openjdk-bazel-commons.sh"
 
 prepare_to_copy_jars
 
+
 copy_cg_manager_jars
 
 copy_event_server_jars
@@ -208,7 +209,6 @@ cp ../../platform-service/config/key.pem .
 cp ../../platform-service/config/cert.pem .
 cp ../../dockerization/platform-service/Dockerfile-platform-service-jenkins-k8-openjdk ./Dockerfile
 cp ../../dockerization/platform-service/Dockerfile-platform-service-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
-cp ../../dockerization/build/Dockerfile-jenkins-slave-portal-jdk-11 ./Dockerfile-jenkins
 cp -r ../../dockerization/platform-service/scripts .
 cp ../../protocol.info .
 echo ${JDK} > jdk.txt
@@ -217,27 +217,6 @@ if [ ! -z ${PURPOSE} ]
 then
     echo ${PURPOSE} > purpose.txt
 fi
-#java -jar platform-service-capsule.jar scan-classpath-metadata
-
-cd ../..
-
-mkdir -p dist/accesscontrol-service
-cd dist/accesscontrol-service
-
-cp ${HOME}/.bazel-dirs/bin/access-control/service/module_deploy.jar accesscontrol-service-capsule.jar
-cp ../../access-control/config/config.yml .
-cp ../../access-control/config/keystore.jks .
-cp ../../access-control/build/container/Dockerfile-accesscontrol-service-jenkins-k8-openjdk ./Dockerfile
-cp ../../access-control/build/container/Dockerfile-accesscontrol-service-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
-cp -r ../../access-control/build/container/scripts/ .
-cp ../../protocol.info .
-echo ${JDK} > jdk.txt
-echo ${VERSION} > version.txt
-if [ ! -z ${PURPOSE} ]
-then
-    echo ${PURPOSE} > purpose.txt
-fi
-#java -jar accesscontrol-service-capsule.jar scan-classpath-metadata
 
 cd ../..
 
@@ -261,92 +240,6 @@ if [ ! -z ${PURPOSE} ]
 then
     echo ${PURPOSE} > purpose.txt
 fi
-#java -jar pipeline-service-capsule.jar scan-classpath-metadata
-
-cd ../..
-
-mkdir -p dist/template-service
-cd dist/template-service
-
-cp ${HOME}/.bazel-dirs/bin/840-template-service/module_deploy.jar template-service-capsule.jar
-cp ../../840-template-service/config.yml .
-cp ../../840-template-service/keystore.jks .
-cp ../../840-template-service/key.pem .
-cp ../../840-template-service/cert.pem .
-cp ../../840-template-service/src/main/resources/redisson-jcache.yaml .
-
-cp ../../dockerization/template-service/Dockerfile-template-service-jenkins-k8-openjdk ./Dockerfile
-cp ../../dockerization/template-service/Dockerfile-template-service-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
-cp -r ../../dockerization/template-service/scripts/ .
-cp ../../protocol.info .
-echo ${JDK} > jdk.txt
-echo ${VERSION} > version.txt
-if [ ! -z ${PURPOSE} ]
-then
-    echo ${PURPOSE} > purpose.txt
-fi
-
-cd ../..
-
-mkdir -p dist/eventsapi-monitor
-cd dist/eventsapi-monitor
-
-cp ${HOME}/.bazel-dirs/bin/950-events-framework-monitor/module_deploy.jar eventsapi-monitor-capsule.jar
-cp ../../950-events-framework-monitor/config.yml .
-cp ../../950-events-framework-monitor/redis/* .
-cp ../../dockerization/eventsapi-monitor/Dockerfile-eventsapi-monitor-jenkins-k8-openjdk ./Dockerfile
-cp ../../dockerization/eventsapi-monitor/Dockerfile-eventsapi-monitor-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
-cp -r ../../dockerization/eventsapi-monitor/scripts/ .
-cp ../../protocol.info .
-echo ${JDK} > jdk.txt
-echo ${VERSION} > version.txt
-if [ ! -z ${PURPOSE} ]
-then
-    echo ${PURPOSE} > purpose.txt
-fi
-
-cd ../..
-
-mkdir -p dist/accesscontrol-service
-cd dist/accesscontrol-service
-
-cp ${HOME}/.bazel-dirs/bin/access-control/service/module_deploy.jar accesscontrol-service-capsule.jar
-cp ../../access-control/service/config.yml .
-cp ../../access-control/service/keystore.jks .
-cp ../../access-control/container/Dockerfile-accesscontrol-service-jenkins-k8-openjdk ./Dockerfile
-cp ../../access-control/container/Dockerfile-accesscontrol-service-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
-cp -r ../../access-control/container/scripts/ .
-cp ../../protocol.info .
-echo ${JDK} > jdk.txt
-echo ${VERSION} > version.txt
-if [ ! -z ${PURPOSE} ]
-then
-    echo ${PURPOSE} > purpose.txt
-fi
-#java -jar accesscontrol-service-capsule.jar scan-classpath-metadata
-
-cd ../..
-
-mkdir -p dist/migrator ;
-cd dist/migrator
-
-cp ${BAZEL_BIN}/100-migrator/module_deploy.jar migrator-capsule.jar
-cp ../../400-rest/src/main/resources/hazelcast.xml .
-cp ../../keystore.jks .
-cp ../../360-cg-manager/key.pem .
-cp ../../360-cg-manager/cert.pem .
-cp ../../360-cg-manager/newrelic.yml .
-cp ../../100-migrator/config.yml .
-cp ../../400-rest/src/main/resources/redisson-jcache.yaml .
-
-cp ../../dockerization/migrator/Dockerfile-manager-jenkins-k8-openjdk ./Dockerfile
-cp ../../dockerization/migrator/Dockerfile-manager-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
-cp -r ../../dockerization/migrator/scripts/ .
-mv scripts/start_process_bazel.sh scripts/start_process.sh
-
-copy_common_files
-
-#java -jar migrator-capsule.jar scan-classpath-metadata
 
 cd ../..
 
@@ -357,7 +250,6 @@ cp ${HOME}/.bazel-dirs/bin/951-debezium-service/module_deploy.jar debezium-servi
 cp ../../951-debezium-service/config.yml .
 cp ../../951-debezium-service/src/main/resources/redisson-jcache.yaml .
 
-cp ../../alpn-boot-8.1.13.v20181017.jar .
 cp ../../dockerization/debezium-service/Dockerfile-debezium-service-jenkins-k8-openjdk ./Dockerfile
 cp ../../dockerization/debezium-service/Dockerfile-debezium-service-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
 cp -r ../../dockerization/debezium-service/scripts/ .
@@ -381,7 +273,6 @@ cp ../../840-template-service/key.pem .
 cp ../../840-template-service/cert.pem .
 cp ../../840-template-service/src/main/resources/redisson-jcache.yaml .
 
-cp ../../alpn-boot-8.1.13.v20181017.jar .
 cp ../../dockerization/template-service/Dockerfile-template-service-jenkins-k8-openjdk ./Dockerfile
 cp ../../dockerization/template-service/Dockerfile-template-service-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
 cp -r ../../dockerization/template-service/scripts/ .
@@ -401,7 +292,6 @@ cd dist/eventsapi-monitor
 cp ${HOME}/.bazel-dirs/bin/950-events-framework-monitor/module_deploy.jar eventsapi-monitor-capsule.jar
 cp ../../950-events-framework-monitor/config.yml .
 cp ../../950-events-framework-monitor/redis/* .
-cp ../../alpn-boot-8.1.13.v20181017.jar .
 cp ../../dockerization/eventsapi-monitor/Dockerfile-eventsapi-monitor-jenkins-k8-openjdk ./Dockerfile
 cp ../../dockerization/eventsapi-monitor/Dockerfile-eventsapi-monitor-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
 cp -r ../../dockerization/eventsapi-monitor/scripts/ .
@@ -419,12 +309,11 @@ mkdir -p dist/accesscontrol-service
 cd dist/accesscontrol-service
 
 cp ${HOME}/.bazel-dirs/bin/access-control/service/module_deploy.jar accesscontrol-service-capsule.jar
-cp ../../access-control/service/config.yml .
-cp ../../access-control/service/keystore.jks .
-cp ../../alpn-boot-8.1.13.v20181017.jar .
-cp ../../access-control/container/Dockerfile-accesscontrol-service-jenkins-k8-openjdk ./Dockerfile
-cp ../../access-control/container/Dockerfile-accesscontrol-service-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
-cp -r ../../access-control/container/scripts/ .
+cp ../../access-control/config/config.yml .
+cp ../../access-control/config/keystore.jks .
+cp ../../access-control/build/container/Dockerfile-accesscontrol-service-jenkins-k8-openjdk ./Dockerfile
+cp ../../access-control/build/container/Dockerfile-accesscontrol-service-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
+cp -r ../../access-control/build/container/scripts/ .
 cp ../../protocol.info .
 echo ${JDK} > jdk.txt
 echo ${VERSION} > version.txt
@@ -432,7 +321,6 @@ if [ ! -z ${PURPOSE} ]
 then
     echo ${PURPOSE} > purpose.txt
 fi
-java -jar accesscontrol-service-capsule.jar scan-classpath-metadata
 
 cd ../..
 
@@ -447,7 +335,6 @@ cp ../../360-cg-manager/cert.pem .
 cp ../../360-cg-manager/newrelic.yml .
 cp ../../100-migrator/config.yml .
 cp ../../400-rest/src/main/resources/redisson-jcache.yaml .
-cp ../../alpn-boot-8.1.13.v20181017.jar .
 
 cp ../../dockerization/migrator/Dockerfile-manager-jenkins-k8-openjdk ./Dockerfile
 cp ../../dockerization/migrator/Dockerfile-manager-jenkins-k8-gcr-openjdk ./Dockerfile-gcr
@@ -455,7 +342,5 @@ cp -r ../../dockerization/migrator/scripts/ .
 mv scripts/start_process_bazel.sh scripts/start_process.sh
 
 copy_common_files
-
-java -jar migrator-capsule.jar scan-classpath-metadata
 
 cd ../..
