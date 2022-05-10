4edabb1bb34fc6d5844678fdf302209c3a64063c
Prateek Barapatre <prateek.barapatre@harness.io>
2022-05-10 19:15:03 +0530

    [fix]: [PL-24523]: ldap setting check

--- 400-rest/src/main/java/software/wings/beans/sso/LdapAuthType.java
+++
@@ -1,7 +0,0 @@
-package software.wings.beans.sso;
-
-import static io.harness.annotations.dev.HarnessTeam.PL;
-
-import io.harness.annotations.dev.OwnedBy;
-
-@OwnedBy(PL) public enum LdapAuthType { INLINE_SECRET, SECRET_REF }
