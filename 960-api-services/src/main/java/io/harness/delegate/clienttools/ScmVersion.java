33816598b4685be8805a4c52e038fe9fcbae8917
Mankrit Singh <mankrit.singh@harness.io>
2022-05-11 10:09:57 +0530

    [PL-24910]: SCM upgrade 1

--- 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
+++ 960-api-services/src/main/java/io/harness/delegate/clienttools/ScmVersion.java
@@ -16,7 +16,7 @@ import org.apache.commons.lang3.StringUtils;
 @Getter
 @Slf4j
 public enum ScmVersion implements ClientToolVersion {
-  DEFAULT("98fc345b");
+  DEFAULT("ac57c254");
 
   private final String version;
 
