c82af37362dea163eff366a053424ffae41cff8b
Ashish Sanodia <ashish.sanodia@harness.io>
2022-05-11 23:46:30 +0530

    [PL-25110]: Fixing OpenTelemetry agent path in services startup

--- dockerization/ci-manager/scripts/start_process.sh
+++ dockerization/ci-manager/scripts/start_process.sh
@@ -48,7 +48,7 @@ export ENABLE_OPENTELEMETRY="true"
 if [[ "${ENABLE_OPENTELEMETRY}" == "true" ]] ; then
     echo "OpenTelemetry is enabled"
     wget https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v1.13.0/opentelemetry-javaagent.jar -O /opt/harness/opentelemetry-javaagent.jar
-    JAVA_OPTS=$JAVA_OPTS" -agentpath:/opt/harness/opentelemetry-javaagent.jar -Dotel.service.name=ci-manager"
+    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/opentelemetry-javaagent.jar -Dotel.service.name=ci-manager"
     echo "Using OpenTelemetry Java Agent"
 fi
 
--- dockerization/pipeline-service/scripts/start_process.sh
+++ dockerization/pipeline-service/scripts/start_process.sh
@@ -44,7 +44,7 @@ export ENABLE_OPENTELEMETRY="true"
 if [[ "${ENABLE_OPENTELEMETRY}" == "true" ]] ; then
     echo "OpenTelemetry is enabled"
     wget https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v1.13.0/opentelemetry-javaagent.jar -O /opt/harness/opentelemetry-javaagent.jar
-    JAVA_OPTS=$JAVA_OPTS" -agentpath:/opt/harness/opentelemetry-javaagent.jar -Dotel.service.name=pipeline-service"
+    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/opentelemetry-javaagent.jar -Dotel.service.name=pipeline-service"
     echo "Using OpenTelemetry Java Agent"
 fi
 
