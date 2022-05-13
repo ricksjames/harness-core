72cf09b851c0881cebdf0f12fe998990658f0b47
Richa Jajoo <richa.jajoo@harness.io>
2022-05-13 18:54:27 +0530

    Update AppServerAgent

--- 310-ci-manager/container/scripts/start_process.sh
+++ 310-ci-manager/container/scripts/start_process.sh
@@ -29,14 +29,13 @@ export GC_PARAMS=" -XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=40 -XX:MaxGCP
 
 export JAVA_OPTS="-Xms${MEMORY}m -Xmx${MEMORY}m -XX:+HeapDumpOnOutOfMemoryError -Xloggc:mygclogfilename.gc $GC_PARAMS"
 
-# TODO: Fix this
-#if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
-#    mkdir /opt/harness/AppServerAgent-20.8.0.30686 && unzip AppServerAgent-20.8.0.30686.zip -d /opt/harness/AppServerAgent-20.8.0.30686
-#    node_name="-Dappdynamics.agent.nodeName=$(hostname)"
-#    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/AppServerAgent-20.8.0.30686/javaagent.jar -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
-#    JAVA_OPTS="$JAVA_OPTS $node_name"
-#    echo "Using Appdynamics java agent"
-#fi
+if [[ "${ENABLE_APPDYNAMICS}" == "true" ]]; then
+    mkdir /opt/harness/AppServerAgent-20.8.0.30686 && unzip AppServerAgent-20.8.0.30686.zip -d /opt/harness/AppServerAgent-20.8.0.30686
+    node_name="-Dappdynamics.agent.nodeName=$(hostname)"
+    JAVA_OPTS=$JAVA_OPTS" -javaagent:/opt/harness/AppServerAgent-20.8.0.30686/javaagent.jar -Dappdynamics.jvm.shutdown.mark.node.as.historical=true"
+    JAVA_OPTS="$JAVA_OPTS $node_name"
+    echo "Using Appdynamics java agent"
+fi
 
 JAVA_BINARY=""
 if type -p java; then
