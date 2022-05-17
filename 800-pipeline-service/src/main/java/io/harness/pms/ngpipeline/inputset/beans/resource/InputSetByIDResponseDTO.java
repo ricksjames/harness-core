1cbc892c4edafa659adeec366ad5da6df7dbc3c1
Naman Verma <naman.verma@harness.io>
2022-05-17 10:57:38 +0530

    unused imports

--- 800-pipeline-service/src/main/java/io/harness/pms/ngpipeline/inputset/beans/resource/InputSetByIDResponseDTO.java
+++ 800-pipeline-service/src/main/java/io/harness/pms/ngpipeline/inputset/beans/resource/InputSetByIDResponseDTO.java
@@ -3,20 +3,11 @@ package io.harness.pms.ngpipeline.inputset.beans.resource;
 import static io.harness.annotations.dev.HarnessTeam.PIPELINE;
 
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.gitsync.sdk.EntityGitDetails;
-import io.harness.gitsync.sdk.EntityValidityDetails;
-import io.harness.pms.inputset.InputSetErrorWrapperDTOPMS;
-import io.harness.pms.inputset.InputSetSchemaConstants;
 import io.harness.pms.ngpipeline.overlayinputset.beans.resource.OverlayInputSetResponseDTOPMS;
-import io.harness.pms.pipeline.PipelineResourceConstants;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.Map;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Value;
