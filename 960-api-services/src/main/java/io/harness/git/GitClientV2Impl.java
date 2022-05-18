d65417c32a42dc15f708ecca2a9ff994640492c5
Harsh Jain <harsh.jain@harness.io>
2022-05-18 14:44:18 +0530

    fix: [CI-4473]: remove timeout

--- 960-api-services/src/main/java/io/harness/git/GitClientV2Impl.java
+++ 960-api-services/src/main/java/io/harness/git/GitClientV2Impl.java
@@ -136,7 +136,7 @@ import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 @OwnedBy(CDP)
 public class GitClientV2Impl implements GitClientV2 {
   private static final int GIT_COMMAND_TIMEOUT = 60;
-  private static final int GIT_COMMAND_RETRY = 2;
+  private static final int GIT_COMMAND_RETRY = 3;
   @Inject private GitClientHelper gitClientHelper;
   /**
    * factory for creating HTTP connections. By default, JGit uses JDKHttpConnectionFactory which doesn't work well with
@@ -337,7 +337,7 @@ public class GitClientV2Impl implements GitClientV2 {
       lsRemoteCommand = (LsRemoteCommand) getAuthConfiguredCommand(lsRemoteCommand, request);
       RetryPolicy<Object> retryPolicy = getRetryPolicyForCommand(format("[Retrying failed git validation, attempt: {}"),
           format("Git validation failed after retrying {} times"));
-      lsRemoteCommand.setTimeout(GIT_COMMAND_TIMEOUT).setRemote(repoUrl).setHeads(true).setTags(true);
+      lsRemoteCommand.setRemote(repoUrl).setHeads(true).setTags(true);
       final LsRemoteCommand finalLsRemoteCommand = lsRemoteCommand;
       Failsafe.with(retryPolicy).get(() -> finalLsRemoteCommand.call());
       log.info(
