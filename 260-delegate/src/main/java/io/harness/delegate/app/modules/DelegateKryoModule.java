44fb83f654fcb05f5d172722b4f0a88a27a49fb3
Johannes Batzill <johannes.batzill@harness.io>
2022-05-20 11:22:17 -0700

    Avoid duplication in DelegateKryoModule

--- 260-delegate/src/main/java/io/harness/delegate/app/modules/DelegateKryoModule.java
+++ 260-delegate/src/main/java/io/harness/delegate/app/modules/DelegateKryoModule.java
@@ -8,65 +8,18 @@
 package io.harness.delegate.app.modules;
 
 import io.harness.govern.ProviderModule;
-import io.harness.serializer.AccessControlClientRegistrars;
-import io.harness.serializer.CgOrchestrationRegistrars;
-import io.harness.serializer.ConnectorBeansRegistrars;
-import io.harness.serializer.CvNextGenCommonsRegistrars;
-import io.harness.serializer.DelegateTaskRegistrars;
-import io.harness.serializer.DelegateTasksBeansRegistrars;
-import io.harness.serializer.FileServiceCommonsRegistrars;
+import io.harness.serializer.DelegateRegistrars;
 import io.harness.serializer.KryoRegistrar;
-import io.harness.serializer.LicenseBeanRegistrar;
-import io.harness.serializer.NGCoreRegistrars;
-import io.harness.serializer.OutboxEventRegistrars;
-import io.harness.serializer.RbacCoreRegistrars;
-import io.harness.serializer.SMCoreRegistrars;
-import io.harness.serializer.kryo.CgOrchestrationBeansKryoRegistrar;
-import io.harness.serializer.kryo.CvNextGenCommonsBeansKryoRegistrar;
-import io.harness.serializer.kryo.DelegateAgentBeansKryoRegister;
-import io.harness.serializer.kryo.DelegateServiceKryoRegister;
-import io.harness.serializer.kryo.EventEntitiesKryoRegistrar;
-import io.harness.serializer.kryo.NgAuthenticationServiceKryoRegistrar;
-import io.harness.serializer.kryo.NotificationBeansKryoRegistrar;
-import io.harness.serializer.kryo.NotificationDelegateTasksKryoRegistrar;
-import io.harness.serializer.kryo.ProjectAndOrgKryoRegistrar;
-import io.harness.serializer.kryo.WatcherBeansKryoRegister;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
-import io.serializer.registrars.NGCommonsRegistrars;
 import java.util.Set;
 
 public class DelegateKryoModule extends ProviderModule {
   @Provides
   @Singleton
   Set<Class<? extends KryoRegistrar> > registrars() {
-    return ImmutableSet.<Class<? extends KryoRegistrar> >builder()
-        .addAll(CvNextGenCommonsRegistrars.kryoRegistrars)
-        .addAll(ConnectorBeansRegistrars.kryoRegistrars)
-        .addAll(DelegateTasksBeansRegistrars.kryoRegistrars)
-        .addAll(CgOrchestrationRegistrars.kryoRegistrars)
-        .add(CgOrchestrationBeansKryoRegistrar.class)
-        .add(ProjectAndOrgKryoRegistrar.class)
-        .addAll(NGCommonsRegistrars.kryoRegistrars)
-        .addAll(NGCoreRegistrars.kryoRegistrars)
-        .addAll(RbacCoreRegistrars.kryoRegistrars)
-        .addAll(SMCoreRegistrars.kryoRegistrars)
-        .addAll(FileServiceCommonsRegistrars.kryoRegistrars)
-        .add(NotificationBeansKryoRegistrar.class)
-        .add(CvNextGenCommonsBeansKryoRegistrar.class)
-        .addAll(LicenseBeanRegistrar.kryoRegistrars)
-        // temporary:
-        .add(NotificationDelegateTasksKryoRegistrar.class)
-        .add(DelegateAgentBeansKryoRegister.class)
-        .add(WatcherBeansKryoRegister.class)
-        .add(DelegateServiceKryoRegister.class)
-        .addAll(OutboxEventRegistrars.kryoRegistrars)
-        .addAll(AccessControlClientRegistrars.kryoRegistrars)
-        .add(EventEntitiesKryoRegistrar.class)
-        .addAll(DelegateTaskRegistrars.kryoRegistrars)
-        .add(NgAuthenticationServiceKryoRegistrar.class)
-        .build();
+    return ImmutableSet.<Class<? extends KryoRegistrar> >builder().addAll(DelegateRegistrars.kryoRegistrars).build();
   }
 }
