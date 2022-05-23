45ba361703a9b5d2f65ac5969236ffed394c9b36
Amit Kumar Das <amit@chaosnative.com>
2022-05-23 11:59:33 +0530

    Updated package name and directory
    Signed-off-by: Amit Kumar Das <amit@chaosnative.com>

--- platform-service/modules/resource-group-service/src/main/java/io/harness/resourcegroup/resourceclient/chaos/ChaosHubResourceImpl.java
+++ platform-service/modules/resource-group-service/src/main/java/io/harness/resourcegroup/resourceclient/chaos/ChaosHubResourceImpl.java
@@ -5,7 +5,7 @@
  * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
  */
 
-package io.harness.resourcegroup.resourceclient.chaoshub;
+package io.harness.resourcegroup.resourceclient.chaos;
 
 import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE;
 import static io.harness.resourcegroup.beans.ValidatorType.BY_RESOURCE_TYPE_INCLUDING_CHILD_SCOPES;
