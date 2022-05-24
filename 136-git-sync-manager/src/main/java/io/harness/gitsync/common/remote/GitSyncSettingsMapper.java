a2457cfdc5c2cdfff1a31663d3d2a4fe204c036a
Mohit Garg <mohit.garg@harness.io>
2022-05-24 12:40:32 +0530

    [PL-25394]: NPE Fix

--- 136-git-sync-manager/src/main/java/io/harness/gitsync/common/remote/GitSyncSettingsMapper.java
+++ 136-git-sync-manager/src/main/java/io/harness/gitsync/common/remote/GitSyncSettingsMapper.java
@@ -30,9 +30,12 @@ public class GitSyncSettingsMapper {
         .accountIdentifier(gitSyncSettings.getAccountIdentifier())
         .orgIdentifier(gitSyncSettings.getOrgIdentifier())
         .projectIdentifier(gitSyncSettings.getProjectIdentifier())
-        .executeOnDelegate(gitSyncSettings.getSettings().get(IS_EXECUTE_ON_DELEGATE).equals(String.valueOf(true)))
-        .isGitSimplificationEnabled(
-            gitSyncSettings.getSettings().get(IS_GIT_SIMPLIFICATION_ENABLED).equals(String.valueOf(true)))
+        .executeOnDelegate(gitSyncSettings.getSettings()
+                               .getOrDefault(IS_EXECUTE_ON_DELEGATE, String.valueOf(false))
+                               .equals(String.valueOf(true)))
+        .isGitSimplificationEnabled(gitSyncSettings.getSettings()
+                                        .getOrDefault(IS_GIT_SIMPLIFICATION_ENABLED, String.valueOf(false))
+                                        .equals(String.valueOf(true)))
         .build();
   }
 
