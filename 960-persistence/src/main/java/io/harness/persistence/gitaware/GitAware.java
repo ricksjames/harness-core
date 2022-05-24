6b342527adb0c70065fcd2f77274ad0db8594956
Naman Verma <naman.verma@harness.io>
2022-05-24 13:45:20 +0530

    remove not needed method

--- 960-persistence/src/main/java/io/harness/persistence/gitaware/GitAware.java
+++ 960-persistence/src/main/java/io/harness/persistence/gitaware/GitAware.java
@@ -8,7 +8,6 @@
 package io.harness.persistence.gitaware;
 
 import io.harness.beans.WithIdentifier;
-import io.harness.gitsync.beans.StoreType;
 import io.harness.persistence.PersistentEntity;
 
 /**
@@ -16,9 +15,6 @@ import io.harness.persistence.PersistentEntity;
  * The SDK should never deal with the entity itself, all the interactions will be driven by the interface
  */
 public interface GitAware extends PersistentEntity, WithIdentifier {
-  // Defines where this entity is stored
-  StoreType getStoreType();
-
   // TODO : As this will be a blob does it make sense for this to be a byte[]
   // The String representation of the resource mostly yaml string
   String getData();
