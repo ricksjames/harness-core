f9457bc70254cdf1a90e88411af8462b0d58584f
Bhavya Agrawal <bhavya.agrawal@harness.io>
2022-05-26 14:01:43 +0530

    [PL-24528]: fix logContextHelper

--- 953-git-sync-commons/src/main/java/io/harness/gitsync/common/helper/GitSyncLogContextHelper.java
+++ 953-git-sync-commons/src/main/java/io/harness/gitsync/common/helper/GitSyncLogContextHelper.java
@@ -4,6 +4,7 @@ import static io.harness.NGCommonEntityConstants.ACCOUNT_KEY;
 import static io.harness.NGCommonEntityConstants.ORG_KEY;
 import static io.harness.NGCommonEntityConstants.PROJECT_KEY;
 import static io.harness.annotations.dev.HarnessTeam.PL;
+import static io.harness.data.structure.EmptyPredicate.isEmpty;
 import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
 
 import io.harness.annotations.dev.OwnedBy;
@@ -17,7 +18,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 @OwnedBy(PL)
 public class GitSyncLogContextHelper {
-  public static final String GIT_OPERATION_TYPE = "GitOperationType";
+  public static final String GIT_OPERATION_TYPE = "gitOperationType";
   public static final String REPO_NAME_KEY = "repoName";
   public static final String FILEPATH_KEY = "filePath";
   public static final String BRANCH_KEY = "branch";
@@ -25,7 +26,7 @@ public class GitSyncLogContextHelper {
 
   public static Map<String, String> setContextMap(Scope scope, String repoName, String branchName, String filePath,
       GitOperation operationType, Map<String, String> contextMap) {
-    if (contextMap != null) {
+    if (isNotEmpty(contextMap)) {
       logContextMap = contextMap;
     }
     setContextIfNotNull(ACCOUNT_KEY, scope.getAccountIdentifier());
