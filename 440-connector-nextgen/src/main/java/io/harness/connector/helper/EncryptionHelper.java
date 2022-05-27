bf5bb490f12a0972ce9c6ffc9c1d90886420cc85
Tejas Tiwari <tejas.tiwari@harness.io>
2022-05-27 12:16:20 +0530

    [PL-25070]: Replaced api call with service call

--- 440-connector-nextgen/src/main/java/io/harness/connector/helper/EncryptionHelper.java
+++ 440-connector-nextgen/src/main/java/io/harness/connector/helper/EncryptionHelper.java
@@ -10,6 +10,7 @@ package io.harness.connector.helper;
 import io.harness.beans.DecryptableEntity;
 import io.harness.ng.core.BaseNGAccess;
 import io.harness.ng.core.NGAccess;
+import io.harness.ng.core.api.NGEncryptedDataService;
 import io.harness.secretmanagerclient.services.api.SecretManagerClientService;
 import io.harness.security.encryption.EncryptedDataDetail;
 
@@ -19,19 +20,19 @@ import java.util.List;
 
 @Singleton
 public class EncryptionHelper {
-  @Inject private SecretManagerClientService ngSecretService;
+  @Inject private NGEncryptedDataService encryptedDataService;
 
   public List<EncryptedDataDetail> getEncryptionDetail(
       DecryptableEntity decryptableEntity, String accountIdentifier, String orgIdentifier, String projectIdentifier) {
     if (decryptableEntity == null) {
       return null;
     }
-    NGAccess basicNGAccessObject = BaseNGAccess.builder()
+    BaseNGAccess basicNGAccessObject = BaseNGAccess.builder()
                                        .accountIdentifier(accountIdentifier)
                                        .orgIdentifier(orgIdentifier)
                                        .projectIdentifier(projectIdentifier)
                                        .build();
 
-    return ngSecretService.getEncryptionDetails(basicNGAccessObject, decryptableEntity);
+    return  encryptedDataService.getEncryptionDetails(basicNGAccessObject, decryptableEntity);
   }
 }
