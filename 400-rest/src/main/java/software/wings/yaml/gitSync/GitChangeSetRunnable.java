0fd70a34a17027025ea7ebed895dcdd28e3d01a1
Abhinav Singh <abhinav.singh@harness.io>
2022-05-31 02:38:20 +0530

    fix: [CDS:37909]: Git sync better retry

--- 400-rest/src/main/java/software/wings/yaml/gitSync/GitChangeSetRunnable.java
+++ 400-rest/src/main/java/software/wings/yaml/gitSync/GitChangeSetRunnable.java
@@ -7,22 +7,28 @@
 
 package software.wings.yaml.gitSync;
 
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Stopwatch;
-import com.google.common.collect.Sets;
-import com.google.inject.Inject;
+import static io.harness.data.structure.EmptyPredicate.isEmpty;
+import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
+import static io.harness.exception.WingsException.ExecutionContext.MANAGER;
+import static io.harness.logging.AutoLogContext.OverrideBehavior.OVERRIDE_ERROR;
+import static io.harness.maintenance.MaintenanceController.getMaintenanceFlag;
+
+import static software.wings.beans.yaml.YamlConstants.GIT_YAML_LOG_PREFIX;
+
+import static java.lang.String.format;
+import static java.util.Collections.singletonList;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.stream.Collectors.toList;
+import static org.mongodb.morphia.aggregation.Accumulator.accumulator;
+import static org.mongodb.morphia.aggregation.Group.first;
+import static org.mongodb.morphia.aggregation.Group.grouping;
+
 import io.harness.exception.WingsException;
 import io.harness.logging.AccountLogContext;
 import io.harness.logging.AutoLogContext;
 import io.harness.logging.ExceptionLogger;
 import io.harness.mongo.ProcessTimeLogContext;
-import lombok.EqualsAndHashCode;
-import lombok.EqualsAndHashCode.Include;
-import lombok.Value;
-import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
-import org.mongodb.morphia.aggregation.Group;
-import org.mongodb.morphia.query.Query;
+
 import software.wings.core.managerConfiguration.ConfigurationController;
 import software.wings.dl.WingsPersistence;
 import software.wings.service.impl.yaml.YamlProcessingLogContext;
@@ -31,6 +37,10 @@ import software.wings.service.intfc.yaml.YamlGitService;
 import software.wings.yaml.gitSync.YamlChangeSet.Status;
 import software.wings.yaml.gitSync.YamlChangeSet.YamlChangeSetKeys;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Stopwatch;
+import com.google.common.collect.Sets;
+import com.google.inject.Inject;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -40,20 +50,13 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
-
-import static io.harness.data.structure.EmptyPredicate.isEmpty;
-import static io.harness.data.structure.EmptyPredicate.isNotEmpty;
-import static io.harness.exception.WingsException.ExecutionContext.MANAGER;
-import static io.harness.logging.AutoLogContext.OverrideBehavior.OVERRIDE_ERROR;
-import static io.harness.maintenance.MaintenanceController.getMaintenanceFlag;
-import static java.lang.String.format;
-import static java.util.Collections.singletonList;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static java.util.stream.Collectors.toList;
-import static org.mongodb.morphia.aggregation.Accumulator.accumulator;
-import static org.mongodb.morphia.aggregation.Group.first;
-import static org.mongodb.morphia.aggregation.Group.grouping;
-import static software.wings.beans.yaml.YamlConstants.GIT_YAML_LOG_PREFIX;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.mongodb.morphia.aggregation.Group;
+import org.mongodb.morphia.query.Query;
 
 /**
  * @author bsollish on 09/26/17
@@ -63,21 +66,14 @@ public class GitChangeSetRunnable implements Runnable {
   public static final List<Status> RUNNING_STATUS_LIST = singletonList(Status.RUNNING);
   //  marking this 1 for now to suit migration. Should be increases to a higher number once migration succeeds
   public static final int MAX_RUNNING_CHANGESETS_FOR_ACCOUNT = 5;
-
-  public static final Integer NO_DELEGATE_FOUND_MAX_RETRY_COUNT = 7;
   private static final AtomicLong lastTimestampForStuckJobCheck = new AtomicLong(0);
   private static final AtomicLong lastTimestampForStatusLogPrint = new AtomicLong(0);
 
-  @Inject
-  private YamlGitService yamlGitSyncService;
-  @Inject
-  private YamlChangeSetService yamlChangeSetService;
-  @Inject
-  private WingsPersistence wingsPersistence;
-  @Inject
-  private ConfigurationController configurationController;
-  @Inject
-  private GitChangeSetRunnableHelper gitChangeSetRunnableHelper;
+  @Inject private YamlGitService yamlGitSyncService;
+  @Inject private YamlChangeSetService yamlChangeSetService;
+  @Inject private WingsPersistence wingsPersistence;
+  @Inject private ConfigurationController configurationController;
+  @Inject private GitChangeSetRunnableHelper gitChangeSetRunnableHelper;
 
   @Override
   public void run() {
